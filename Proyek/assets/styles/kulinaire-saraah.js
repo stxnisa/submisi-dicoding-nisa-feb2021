f6e74193a124a9429a25ad09cc07532ed3c4958a
stxnisa <73210237+stxnisa@users.noreply.github.com>
2021-09-07 17:19:22 +0700

    Unggahan awal - 7 September 2021

---
+++ Proyek/assets/styles/kulinaire-saraah.js
@@ -0,0 +1,41 @@
+let siwang = document.querySelector("#siwang");
+siwang.setAttribute("src", "assets/image/sambel-siwang-ori.png");
+
+let mercon = document.querySelector("#mercon");
+mercon.setAttribute("src", "assets/image/sambel-mercon.png");
+
+let hejau = document.querySelector("#hejau");
+hejau.setAttribute("src", "assets/image/sambel-hejau.png");
+
+let cumi = document.querySelector("#cumi");
+cumi.setAttribute("src", "assets/image/sambel-siwang-udang.png");
+
+let teri = document.querySelector("#teri");
+teri.setAttribute("src", "assets/image/sambel-siwang-teri.png");
+
+let ikan = document.querySelector("#ikan");
+ikan.setAttribute("src", "assets/image/sambel-siwang-ikan.png");
+
+let telur = document.querySelector("#telur");
+telur.setAttribute("src", "assets/image/sambel-siwang-telur-asin.png");
+
+let organik = document.querySelector("#organik");
+organik.setAttribute("src", "assets/image/koya-ta-organik.png");
+
+let bputih = document.querySelector("#bputih");
+bputih.setAttribute("src", "assets/image/koya-ta-bputih.png");
+
+let balado = document.querySelector("#balado");
+balado.setAttribute("src", "assets/image/koya-ta-balado.png");
+
+let ori = document.querySelector("#ori");
+ori.setAttribute("src", "assets/image/susu-ori.png");
+
+let nonsusu = document.querySelector("#nonsusu");
+nonsusu.setAttribute("src", "assets/image/susu-non-susu.png");
+
+let nongula = document.querySelector("#nongula");
+nongula.setAttribute("src", "assets/image/susu-non-milk.png");
+
+let nisa = document.querySelector("#nisa");
+nisa.setAttribute("src", "assets/image/nisa.png");
